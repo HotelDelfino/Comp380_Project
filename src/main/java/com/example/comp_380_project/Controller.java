5377f7790a375dccc87d842ad63a943395b8ea57
AngelV200 <140028970+AngelV200@users.noreply.github.com>
2023-07-22 17:56:57 -0700

    changed titles

--- src/main/java/com/example/comp_380_project/Controller.java
+++ src/main/java/com/example/comp_380_project/Controller.java
@@ -1,5 +1,7 @@
 package com.example.comp_380_project;
 
+import java.util.*;
+import java.io.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -13,10 +15,18 @@ public class Controller {
     private Label nameLabel;
 
     @FXML
-    protected void handleSubmitButtonAction() {System.out.println("Logging in Boss...");}
+    protected void handleSubmitButtonAction() {
+        
+        System.out.println("Logging in Boss...");
+    }
 
     @FXML
-    protected void onSignInManagerButton(){System.out.println("OK boss");}
+    protected void onSignInManagerButton() {
+        System.out.println("OK boss");
+    }
+
     @FXML
-    protected void onRegisterButton(){System.out.println("Register boss");}
+    protected void onRegisterButton() {
+        System.out.println("Register boss");
+    }
 }
--- src/main/java/com/example/comp_380_project/JavaFXApplication.java
+++ src/main/java/com/example/comp_380_project/JavaFXApplication.java
@@ -1,24 +1,24 @@
 package com.example.comp_380_project;
 
+import java.util.*;
+import java.io.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class JavaFXApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JavaFXApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hotel");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         // This starts the GUI 
-        //launch();
+        launch();
     }
 }
