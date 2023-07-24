ea0f804fd37a83a228d9e5031308d0f99e0192b1
AngelV200 <140028970+AngelV200@users.noreply.github.com>
2023-07-23 21:49:45 -0700

    New GUI with password
    For Guest and Manager cleaned up main as well.

---
+++ src/main/java/com/example/comp_380_project/Main.java
@@ -0,0 +1,10 @@
+package com.example.comp_380_project;
+
+public class Main {
+    public static void main(String[] args) {
+        // This starts the GUI 
+        //launch();
+        JavaFXApp app = new JavaFXApp();
+        app.runApp();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.comp_380_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.comp_380_project to javafx.fxml;
