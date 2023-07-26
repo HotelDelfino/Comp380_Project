bb98914cd437630c9f6d7a30303e01958752c4dd
Zucaritas46 <105331520+Zucaritas46@users.noreply.github.com>
2023-07-26 10:57:14 -0700

    Added new functions to file reader
    the database can now store all usernames and passwords in an array, and verify if they exist in the array.

--- src/main/java/com/example/comp_380_project/FileReader.java
+++ src/main/java/com/example/comp_380_project/FileReader.java
@@ -7,15 +7,37 @@ import java.util.ArrayList;
 
 public class FileReader {
 
-    ArrayList<String> userNames = new ArrayList<>();
-    ArrayList<String> passWords = new ArrayList<>();
-    /** This reads text file. Does not do anything else **/
-    public void readTextFile(File file){
+    static File usernamesFile;
+    static File passwordFile;
+    static ArrayList<String> userNames = new ArrayList<>();
+    static ArrayList<String> passwords = new ArrayList<>();
+
+    /** This reads username text file. Adds content to Array **/
+    public void readUsernameFile(File file){
 
         try{
             BufferedReader br = new BufferedReader(new java.io.FileReader(file));
             String st;
             while ((st = br.readLine())!= null){
+                userNames.add(st);
+                System.out.println(st);
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+            e.printStackTrace();
+        }catch(IOException f){
+            System.out.println("An Error has occurred");
+            f.printStackTrace();
+        }
+    }
+    /** This reads password text file. Adds content to Array **/
+    public void readPasswordFile(File file){
+
+        try{
+            BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+            String st;
+            while ((st = br.readLine())!= null){
+                passwords.add(st);
                 System.out.println(st);
             }
         }catch(FileNotFoundException e){
@@ -27,12 +49,18 @@ public class FileReader {
         }
     }
 
+    /** This creates a new username and password **/
+    public void registerNewUser(String username, String pw){
+        writeTextFile(usernamesFile, username);
+        writeTextFile(passwordFile, pw);
+    }
+
     /** This writes a new line on the text file. Does not do anything else**/
-    public void writeTextFile(File file, String username){
+    public void writeTextFile(File file, String stuff){
 
         try{
             FileWriter myWriter = new FileWriter(file,true);
-            myWriter.write(username + "\n");
+            myWriter.write(stuff + "\n");
             myWriter.close();
             System.out.println("File Writing Successful");
         }catch(IOException e){
@@ -50,18 +78,23 @@ public class FileReader {
                 System.out.println("File Successfully Created: " + user.getName());
             }else{
                 System.out.println("File usernames.text already exists");
+                // If file exists, then it will read it and put contents in array
+                readUsernameFile(user);
             }
+            //Save the file for the whole class to use
+            usernamesFile = user;
+
             File passW = new File("passwords.txt");
             if(passW.createNewFile()){
                 System.out.println("File Successfully Created: " + passW.getName());
             }else{
                 System.out.println("File passwords.txt already exists");
+                readPasswordFile(passW);
             }
-            /** //Example
-            String username = "Jose";
-            writeTextFile(obj, username);
-            readTextFile(obj);
-             **/
+            //Save the file for the whole class to use
+            passwordFile = passW;
+
+
         }catch(IOException e){
             System.out.println("An Error Occurred");
             e.printStackTrace();
@@ -74,5 +107,27 @@ public class FileReader {
         // Temporary
         System.out.println(path.toAbsolutePath());
         return path.toAbsolutePath();
+    }
+    
+    /** Checks to see if Username exists **/
+    public boolean verifyUsername(String userN){
+        for (int i = 0; i < userNames.size(); i++) {
+            if (userN.equals(userNames.get(i))) return true;
+        }
+        return false;
+    }
+
+    /** Checks to see if Username is in the system and if the password matches **/
+    public boolean verifyLogin(String userN, String pW){
+        for (int i = 0; i < userNames.size(); i++) {
+            if (userN.equals(userNames.get(i))) {
+                return verifyPassWord(pW,i);
+            }
+        }
+        return false;
+    }
+
+    public boolean verifyPassWord(String pW, int index){
+        return pW.equals(passwords.get(index));
     }
 }
