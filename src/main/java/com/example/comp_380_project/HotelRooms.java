a2dae98f111787ff9e326ec76192500641a2f433
Doopliss <sergio.dieguez.216@my.csun.edu>
2023-07-26 16:00:08 -0700

    Updated Strings 1

--- src/main/java/com/example/comp_380_project/HotelRooms.java
+++ src/main/java/com/example/comp_380_project/HotelRooms.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class HotelRooms {
 
-    String[] floor1 = new String[10];
-    String[] floor2 = new String[10];
-    String[] floor3 = new String[10];
-    String[] floor4 = new String[10];
+    String[] floor1 = new String[10]; //Small rooms
+    String[] floor2 = new String[10]; //Larger rooms
+    String[] floor3 = new String[10]; //Family rooms
+    String[] floor4 = new String[10]; //Luxury rooms
+    String[] floor5 = new String[10]; //Penthouse suite
 
     Scanner input = new Scanner(System.in);
 
-
+    
 }
