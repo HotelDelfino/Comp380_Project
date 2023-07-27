3953d91dddd653cf446c45aade11689d30e61b16
Doopliss <sergio.dieguez.216@my.csun.edu>
2023-07-26 22:04:51 -0700

    changes to code

--- src/main/java/com/example/comp_380_project/HotelRooms.java
+++ src/main/java/com/example/comp_380_project/HotelRooms.java
@@ -1,5 +1,6 @@
 package com.example.comp_380_project;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class HotelRooms {
@@ -10,7 +11,15 @@ public class HotelRooms {
     String[] floor4 = new String[10]; //Luxury rooms
     String[] floor5 = new String[10]; //Penthouse suite
 
+    /* Going crazy with these changes
+    Obj[] floor1 = new Obj[]; //Small rooms
+    Obj[] floor2 = new Obj[]; //Larger rooms
+    Obj[] floor3 = new Obj[]; //Family rooms
+    Obj[] floor4 = new Obj[]; //Luxury rooms
+    Obj[] floor5 = new Obj[]; //Penthouse suite
+     */
+
     Scanner input = new Scanner(System.in);
 
-    
+
 }
