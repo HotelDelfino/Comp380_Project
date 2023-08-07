1a3795ec1bc52f4227458616ef35708a2c549357
Zucaritas46 <105331520+Zucaritas46@users.noreply.github.com>
2023-08-07 12:21:00 -0700

    added more functionallity to Guest
    Added a constructor to Guest. Added methods to Guest inpreperation to integrate with HotelRooms backend

--- src/main/java/com/example/comp_380_project/Guest.java
+++ src/main/java/com/example/comp_380_project/Guest.java
@@ -1,9 +1,19 @@
 package com.example.comp_380_project;
 
+import java.util.ArrayList;
+
 public class Guest {
     private String username;
     private String password;
     private String Name;
+    private ArrayList<Integer> bookedReservations = new ArrayList<>();
+
+    Guest(){}
+    Guest(String un, String pw){
+        this.username = un;
+        this.password = pw;
+        this.bookedReservations.add(0);
+    }
 
     void setUsername(String UserName){
         this.username = UserName;
@@ -12,13 +22,25 @@ public class Guest {
         this.password = password;
     }
     boolean isCorrectPW(String pw){
-        return this.password == pw;
+        return this.password.equals(pw);
     }
-    void menu(){
-        System.out.println("1. Reserve a room ");
-        System.out.println("2. Cancel a reservation ");
-        System.out.println("3. View reservations ");
-        System.out.println("0. Logout ");
+    /** Checks if any rooms are booked **/
+    void hasRooms(){
+
     }
+    void addReservation(int room){
+        bookedReservations.add(room);
+    }
+    void cancelReservation(int room){
+        for(int i = 0; i< bookedReservations.size();i++) {
+            if (bookedReservations.get(i) == room) {
+                bookedReservations.remove(i);
+            }else {
+                System.out.println("No reservation at " + room + " exists.");
+            }
+        }
+    }
+    // Need HotelRooms Finished
+    void modifyReservations(int room){}
 
 }
