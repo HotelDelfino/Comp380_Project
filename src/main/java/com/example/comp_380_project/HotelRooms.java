d22670d6a4a12eefe9658a8c094c8ebba6577c06
Doopliss <sergio.dieguez.216@my.csun.edu>
2023-08-07 03:17:45 -0700

    Finished the majority of testing
    I wanted to test my own code before inserting it to the main file. Mostly done with the changeRoom method not fully finished.

--- src/main/java/com/example/comp_380_project/HotelRooms.java
+++ src/main/java/com/example/comp_380_project/HotelRooms.java
@@ -5,23 +5,196 @@ import java.util.Scanner;
 
 public class HotelRooms {
 
-    String[] floor1 = new String[10]; //Small rooms
-    String[] floor2 = new String[10]; //Larger rooms
-    String[] floor3 = new String[10]; //Family rooms
-    String[] floor4 = new String[10]; //Luxury rooms
-    String[] floor5 = new String[10]; //Penthouse suite
-
-    /* Going crazy with these changes
-    Obj[] floor1 = new Obj[]; //Small rooms
-    Obj[] floor2 = new Obj[]; //Larger rooms
-    Obj[] floor3 = new Obj[]; //Family rooms
-    Obj[] floor4 = new Obj[]; //Luxury rooms
-    Obj[] floor5 = new Obj[]; //Penthouse suite
-     */
-
-    Scanner input = new Scanner(System.in);
+    String[] floor1 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Small rooms
+    String[] floor2 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Larger rooms
+    String[] floor3 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Family rooms
+    String[] floor4 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Luxury rooms
+    String[] floor5 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Penthouse suite
 
 
+    public void reserveRoom(int floor, int roomNum) {
+        if (floor >= 1 && floor <= 5 && roomNum >= 1 && roomNum <= 10) {
+            if (floor == 1) {
+                if (floor1[roomNum - 1] != "Closed") {
+                    floor1[roomNum - 1] = "Closed";
+                    System.out.println();
+                    System.out.println("Thank you for booking room " + roomNum + " on floor 1!");
+                } else {
+                    System.out.println("Sorry but this room is already book! Please try another room.");
+                }
+            }
+            if (floor == 2) {
+                if (floor2[roomNum - 1] != "Closed") {
+                    floor2[roomNum - 1] = "Closed";
+                    System.out.println();
+                    System.out.println("Thank you for booking room " + roomNum + " on floor 2!");
+                } else {
+                    System.out.println("Sorry but this room is already book! Please try another room.");
+                }
+            }
+            if (floor == 3) {
+                if (floor3[roomNum - 1] != "Closed") {
+                    floor3[roomNum - 1] = "Closed";
+                    System.out.println();
+                    System.out.println("Thank you for booking room " + roomNum + " on floor 3!");
+                } else {
+                    System.out.println("Sorry but this room is already book! Please try another room.");
+                }
+            }
+            if (floor == 4) {
+                if (floor4[roomNum - 1] != "Closed") {
+                    floor4[roomNum - 1] = "Closed";
+                    System.out.println();
+                    System.out.println("Thank you for booking room " + roomNum + " on floor 4!");
+                } else {
+                    System.out.println("Sorry but this room is already book! Please try another room.");
+                }
+            }
+            if (floor == 5) {
+                if (floor2[roomNum - 1] != "Closed") {
+                    floor2[roomNum - 1] = "Closed";
+                    System.out.println();
+                    System.out.println("Thank you for booking room " + roomNum + " on floor 5!");
+                } else {
+                    System.out.println("Sorry but this room is already book! Please try another room.");
+                }
+            }
 
+        } else {
+            System.out.println();
+            System.out.println("You inputted a wrong number for either floor or room number. Please try again!");
+        }
+    }
 
+    public void cancelRoom(int floor, int roomNum) {
+        if (floor >= 1 && floor <= 5 && roomNum >= 1 && roomNum <= 10) {
+            if (floor == 1) {
+                if (floor1[roomNum - 1] != "Open") {
+                    floor1[roomNum - 1] = "Open";
+                    System.out.println();
+                    System.out.println("Thank you for letting us know! Your refund should be given to you in 48 hours!");
+                } else {
+                    System.out.println("Sorry but this room is not under you name. Please try again.");
+                }
+            }
+            if (floor == 2) {
+                if (floor2[roomNum - 1] != "Open") {
+                    floor2[roomNum - 1] = "Open";
+                    System.out.println();
+                    System.out.println("Thank you for letting us know! Your refund should be given to you in 48 hours!");
+                } else {
+                    System.out.println("Sorry but this room is not under you name. Please try again.");
+                }
+            }
+            if (floor == 3) {
+                if (floor3[roomNum - 1] != "Open") {
+                    floor3[roomNum - 1] = "Open";
+                    System.out.println();
+                    System.out.println("Thank you for letting us know! Your refund should be given to you in 48 hours!");
+                } else {
+                    System.out.println("Sorry but this room is not under you name. Please try again.");
+                }
+            }
+            if (floor == 4) {
+                if (floor4[roomNum - 1] != "Open") {
+                    floor4[roomNum - 1] = "Open";
+                    System.out.println();
+                    System.out.println("Thank you for letting us know! Your refund should be given to you in 48 hours!");
+                } else {
+                    System.out.println("Sorry but this room is not under you name. Please try again.");
+                }
+            }
+            if (floor == 5) {
+                if (floor2[roomNum - 1] != "Open") {
+                    floor2[roomNum - 1] = "Open";
+                    System.out.println();
+                    System.out.println("Thank you for letting us know! Your refund should be given to you in 48 hours!");
+                } else {
+                    System.out.println("Sorry but this room is not under you name. Please try again.");
+                }
+            }
+        }else {
+            System.out.println("You inputted a wrong number for either floor or room number. Please try again!");
+        }
+    }
+
+    public void changeRoom(int floor, int roomNum){
+        if (floor >= 1 && floor <= 5 && roomNum >= 1 && roomNum <= 10) {
+            cancelRoom(floor, roomNum);
+        } else {
+            System.out.println("You inputted a wrong number for either floor or room number. Please try again!");
+        }
+    }
+
+    public void searchRoom(int floor, int roomNum){
+        if (floor >= 1 && floor <= 5 && roomNum >= 1 && roomNum <= 10) {
+            if (floor == 1) {
+                if (floor1[roomNum - 1] != "Closed") {
+                    System.out.println("The room " + roomNum + " on floor 1 is open!");
+                } else {
+                    System.out.println("Room " + roomNum + " on floor 1 has already been taken by someone else!\n" +
+                            "Please look for another room if possible. If none available, please choose another floor for possible availability!");
+                }
+            }
+            if (floor == 2) {
+                if (floor2[roomNum - 1] != "Closed") {
+                    System.out.println("The room " + roomNum + " on floor 2 is open!");
+                } else {
+                    System.out.println("This room " + roomNum + " on floor 2 has already been taken by someone else!\n" +
+                            "Please look for another room if possible. If none available, please choose another floor for possible availability!");
+                }
+            }
+            if (floor == 3) {
+                if (floor3[roomNum - 1] != "Closed") {
+                    System.out.println("The room " + roomNum + " on floor 3 is open!");
+                } else {
+                    System.out.println("This room " + roomNum + " on floor 3 has already been taken by someone else!\n" +
+                            "Please look for another room if possible. If none available, please choose another floor for possible availability!");
+                }
+            }
+            if (floor == 4) {
+                if (floor4[roomNum - 1] != "Closed") {
+                    System.out.println("The room " + roomNum + " on floor 4 is open!");
+                } else {
+                    System.out.println("This room " + roomNum + " on floor 4 has already been taken by someone else!\n" +
+                            "Please look for another room if possible. If none available, please choose another floor for possible availability!");
+                }
+            }
+            if (floor == 5) {
+                if (floor5[roomNum - 1] != "Closed") {
+                    System.out.println("The room " + roomNum + " on floor 5 is open!");
+                } else {
+                    System.out.println("This room " + roomNum + " on floor 5 has already been taken by someone else! Please look for" +
+                            " another room if possible. If none available, please choose another floor for possible availability!");
+                }
+            }
+        } else {
+            System.out.println("You inputted a wrong number for either floor or room number. Please try again!");
+        }
+    }
+
+    public void roomInfo(int floor){
+        if (floor >= 1 && floor <= 5) {
+            if (floor == 1) {
+                System.out.println("Floor 1 have our finest small rooms, perfect for 1-2 people to enjoy!");
+            }
+            if (floor == 2) {
+                System.out.println("For those with a group of 3-4 people, our second floor has the right open space for you!");
+            }
+            if (floor == 3) {
+                System.out.println("Having a big family trip? Well on floor 3 we have just the room for you!\n" +
+                        "Perfect for families of 5-6 people. Baby station included!");
+            }
+            if (floor == 4) {
+                System.out.println("Our rooms not good enough for your standards? Don't worry we got something for you as well!\n" +
+                        "On the fourth floor we have some luxurious rooms for those who love interior design.");
+            }
+            if (floor == 5) {
+                System.out.println("Can't stand the closed spaces of a normal hotel room? \n" +
+                        "On the top floor awaits your spacious and luxurious penthouse suite! Fit for a king!");
+            }
+        } else {
+            System.out.println("You inputted a wrong number for either floor or room number. Please try again!");
+        }
+    }
 }
--- src/main/java/com/example/comp_380_project/Main.java
+++ src/main/java/com/example/comp_380_project/Main.java
@@ -1,5 +1,5 @@
 package com.example.comp_380_project;
-
+import java.util.Scanner;
 import java.util.*;
 import java.io.*;
 
@@ -12,5 +12,97 @@ public class Main {
         JavaFXApp app = new JavaFXApp(dataBase, optionMenu, args);
         
         app.runApp();
+
+        Scanner input = new Scanner(System.in);
+        HotelRooms hotel = new HotelRooms();
+
+        System.out.println();
+        System.out.print("Welcome to Summer's Hotel!");
+        while(true){
+            System.out.println("\n");
+            System.out.println("Please select one of these options:\n" +
+                    "Reserve a room: 1\n" +
+                    "Cancel a room: 2\n" +
+                    "Change your reservation: 3\n" +
+                    "Search for a Reservation: 4\n" +
+                    "Room Info: 5\n" +
+                    "Quit: 6\n");
+
+
+            switch (input.nextLine()) {
+                case "1":
+                    Scanner input1 = new Scanner(System.in);
+                    System.out.println();
+                    System.out.println("Please select which type of room you would want to stay in and the room" +
+                            " number you would like to stay in:");
+                    System.out.println();
+                    System.out.println("Please note that each floor has ten rooms\n\n" +
+                            "Small rooms: 1\n" +
+                            "Larger rooms: 2\n" +
+                            "Family rooms: 3\n" +
+                            "Luxury rooms: 4\n" +
+                            "Penthouse suite: 5\n\n");
+                    System.out.println();
+                    hotel.reserveRoom(input1.nextInt(), input1.nextInt());
+                    break;
+                case "2":
+                    Scanner input2 = new Scanner(System.in);
+                    System.out.println("I'm so sorry to hear that.\n" +
+                            "Please tell me the floor ad room number you reserved in:\n");
+                    System.out.println("Please note that each floor has ten rooms\n\n" +
+                            "Small rooms: 1\n" +
+                            "Larger rooms: 2\n" +
+                            "Family rooms: 3\n" +
+                            "Luxury rooms: 4\n" +
+                            "Penthouse suite: 5\n");
+                    System.out.println();
+                    hotel.cancelRoom(input2.nextInt(), input2.nextInt());
+                    break;
+                case "3":
+                    Scanner input3 = new Scanner(System.in);
+                    System.out.println("Please input the floor and room number you would like to change:\n");
+                    System.out.println("Please note that each floor has ten rooms\n\n" +
+                            "Small rooms: 1\n" +
+                            "Larger rooms: 2\n" +
+                            "Family rooms: 3\n" +
+                            "Luxury rooms: 4\n" +
+                            "Penthouse suite: 5\n");
+                    hotel.changeRoom(input3.nextInt(), input3.nextInt());
+
+                    Scanner input4 = new Scanner(System.in);
+                    System.out.print("Which room would you like to change it to?");
+                    hotel.reserveRoom(input4.nextInt(), input4.nextInt());
+                    break;
+                case "4":
+                    Scanner input5 = new Scanner(System.in);
+                    System.out.println("Please input the room you would like to know the status of:\n ");
+                    System.out.println("Please note that each floor has ten rooms\n\n" +
+                            "Small rooms: 1\n" +
+                            "Larger rooms: 2\n" +
+                            "Family rooms: 3\n" +
+                            "Luxury rooms: 4\n" +
+                            "Penthouse suite: 5\n");
+                    hotel.searchRoom(input5.nextInt(), input5.nextInt());
+                    break;
+                case "5":
+                    Scanner input6 = new Scanner(System.in);
+                    System.out.println("Which floor would you like to know more about?\n");
+                    System.out.println("Small rooms: 1\n" +
+                            "Larger rooms: 2\n" +
+                            "Family rooms: 3\n" +
+                            "Luxury rooms: 4\n" +
+                            "Penthouse suite: 5\n");
+                    hotel.roomInfo(input6.nextInt());
+                    break;
+                case "6":
+                    System.out.println("Thank you for stopping by! Have a wonderful day!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("It seems that you inputted an incorrect input, please try again!");
+                    System.out.println();
+            }
+        }
     }
 }
