9dc31f0264d2881d30c04fe827d41ad425437fb8
Zucaritas46 <105331520+Zucaritas46@users.noreply.github.com>
2023-08-15 19:27:48 -0700

    Documentation

--- src/main/java/com/example/comp_380_project/FileReader.java
+++ src/main/java/com/example/comp_380_project/FileReader.java
@@ -5,9 +5,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-/** @author Jorge Enriquez
+/**
+ * FileReader
+ * The FileReader class creates 4 textfiles that hold usernames, passwords, reservations and reviews.
+ * FileReader is in charge of reading these 4 text files and holding their contents within the class.
+ * FileReader is also able to modify the information within these arrays and write to the textfiles
+ *
+ * @author Jorge Enriquez
  *  Date: Aug 15, 2023
- *  Version: */
+ *  Version: 1.0*/
 
 public class FileReader{
     private File usernamesFile;
@@ -21,15 +27,16 @@ public class FileReader{
     private ArrayList<String> reviews = new ArrayList<>();
     private ArrayList<Guest> registeredMembers = new ArrayList<>();
     
-    /** @param hotelrooms
-     * Constructor. Starts the Textfile. ONLY CALL ONCE **/
+    /** Constructor. Starts the Textfile. ONLY CALL ONCE
+     * @param hotelrooms gets the HotelRooms object */
     FileReader(HotelRooms hotelrooms){
         hotel = hotelrooms;
         verifyTextFile();
         createRegisteredGuest();
     }
-    /**@param file
-     * reads the username file and adds it to the userNames array*/
+    /** reads the username file and adds it to the userNames array
+     * @param file gets the File necessary to run
+     * @exception FileNotFoundException On file not found. */
     private void readUsernameFile(File file){
         System.out.println("Attempting to read file: " + file.getAbsolutePath());
         try{
@@ -47,8 +54,9 @@ public class FileReader{
             f.printStackTrace();
         }
     }
-    /** @param file
-     * This reads password text file. Adds content to Array **/
+    /** This reads password text file. Adds content to Array
+     * @param file gets the File necessary to run
+     * @exception FileNotFoundException On file not found. */
     private void readPasswordFile(File file){
 
         try{
@@ -67,8 +75,9 @@ public class FileReader{
         }
     }
 
-    /** @param file
-     * This reads the reservations file. Adds to HotelRooms **/
+    /** This reads the reservations file. Adds to HotelRooms
+     * @param file gets the File necessary to run
+     * @exception FileNotFoundException On file not found. */
     private void readReservationsFile(File file){
 
         try{
@@ -87,8 +96,9 @@ public class FileReader{
         }
     }
 
-    /** @param file
-     * This reads the reviews files and stores it in Reviews Array */
+    /** This reads the reviews files and stores it in Reviews Array
+     * @param file gets the File necessary to run
+     * @exception FileNotFoundException On file not found. */
     private void readReviewFile(File file){
         try{
             BufferedReader br = new BufferedReader(new java.io.FileReader(file));
@@ -105,22 +115,20 @@ public class FileReader{
         }
     }
 
-    /** @param review
-     * Adds to Review Array and writes to review Textfile*/
+    /** Adds to Review Array and writes to review Textfile
+     * @param review gets a String */
     public void addReview(String review){
         reviews.add(review);
         writeTextFile(reviewFile,review);
     }
 
-    /** @return ArrayList
-     *
-     * This function allows access to the reviews array
-     * */
+    /** This function allows access to the reviews array
+     * @return ArrayList returns array list to use */
     public ArrayList<String> getReviews(){return reviews;}
 
-    /** @param username
-     * @param pw
-     * This creates a new username and password **/
+    /** This creates a new username and password
+     * @param username gets a String username
+     * @param pw gets a String pw */
     public void registerNewUser(String username, String pw){
         writeTextFile(usernamesFile, username);
         userNames.add(username);
@@ -130,9 +138,9 @@ public class FileReader{
         createGuest(username,pw);
     }
 
-    /** @param file
-     * @param stuff
-     * This writes a new line on the text file. Does not do anything else**/
+    /** This writes a new line on the text file. Does not do anything else
+     * @param file gets a File object
+     * @param stuff gets a String of stuff*/
     private void writeTextFile(File file, String stuff){
 
         try{
@@ -145,16 +153,16 @@ public class FileReader{
             e.printStackTrace();
         }
     }
-    /** @param stuff
-     * @param guestIndex
-     * Can be called by other classes without a File object. Uses File object attached to FileReader */
+    /** Can be called by other classes without a File object. Uses File object attached to FileReader.
+     * @param stuff gets a String of stuff
+     * @param guestIndex gets an int*/
     public void updateReservationTextFile(String stuff, int guestIndex){
         writeReservationTextFile(reservationsFile,stuff,guestIndex);
     }
-    /** @param file
-     * @param stuff
-     * @param guestIndex
-     * This writes on the reservations text file. Uses the guest index to find where to override text **/
+    /** This writes on the reservations text file. Uses the guest index to find where to override text
+     * @param file gets a File object
+     * @param stuff gets a String of stuff
+     * @param guestIndex gets an int*/
     private void writeReservationTextFile(File file, String stuff, int guestIndex){
 
         // Checks to see if the array is more than one to avoid out of bounds exception
@@ -191,7 +199,7 @@ public class FileReader{
     }
 
 
-    /** This verifies that a text file exists. Otherwise, creates one. **/
+    /** This verifies that a text file exists, otherwise, creates one. **/
     public void verifyTextFile(){
 
         try{
@@ -240,9 +248,10 @@ public class FileReader{
         }
     }
 
-    /** @return String
+    /** Captures and returns file path for Username.txt
+     * @return String returns the file path as a string
      *
-     * Captures and returns file path for Username.txt */
+     * */
     public String getUserFilePath(){
         Path path = Paths.get("usernames.txt");
         // Temporary
@@ -250,8 +259,8 @@ public class FileReader{
         return path.toAbsolutePath().toString();
     }
 
-    /** @return String
-     * Capture password file path **/
+    /** Capture password file path
+     * @return String String returns the file path as a string*/
     public String getPassWFilePath(){
         Path path = Paths.get("passwords.txt");
         // Temporary
@@ -259,8 +268,10 @@ public class FileReader{
         return path.toAbsolutePath().toString();
     }
     
-    /** @return String
-     * Checks to see if Username exists **/
+    /** Checks to see if Username exists
+     * @param userN gets a String
+     * @return boolean returns true or false
+     *  **/
     public boolean verifyUsername(String userN){
         for (int i = 0; i < userNames.size(); i++) {
             if (userN.equals(userNames.get(i))) return true;
@@ -268,10 +279,11 @@ public class FileReader{
         return false;
     }
 
-    /** @param userN
-     * @param pW
-     * @return boolean
-     * Checks to see if Username is in the system and if the password matches **/
+    /** Checks to see if Username is in the system and if the password matches
+     * @param userN gets a String
+     * @param pW gets a String
+     * @return boolean returns true or false
+     *  **/
     public boolean verifyLogin(String userN, String pW){
         for (int i = 0; i < userNames.size(); i++) {
             if (userN.equals(userNames.get(i))) {
@@ -281,16 +293,16 @@ public class FileReader{
         return false;
     }
 
-    /** @param pW
-     * @param index
-     * @return boolean
-     * Returns a boolean value by checking password with */
+    /** Returns a boolean value by checking password with
+     * @param pW gets a string
+     * @param index gets a string
+     * @return boolean returns true or false*/
     public boolean verifyPassWord(String pW, int index){
         return pW.equals(passwords.get(index));
     }
-    /** @param username
-     *  @param password
-     *  This function instanciates a new guest and adds it to registeredMembers array */
+    /** This function instantiates a new guest and adds it to registeredMembers array
+     * @param username gets a String
+     *  @param password */
     private void createGuest(String username, String password){
         registeredMembers.add(new Guest(username,password));
     }
@@ -304,9 +316,9 @@ public class FileReader{
         }
     }
 
-    /** @param username
-     *  @return Guest
-     *  This function returns a guest object from the registedMembers array by looking through the usernames array*/
+    /** This function returns a guest object from the registedMembers array by looking through the usernames array
+     * @param username get string
+     *  @return Guest return Guest object*/
     public Guest getRegisteredGuest(String username){
         for (int i = 0; i < userNames.size(); i++) {
             if (userNames.get(i).equals(username)) return registeredMembers.get(i);
