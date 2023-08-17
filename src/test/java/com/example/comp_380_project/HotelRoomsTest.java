82c9872d3b97143a23015a95f5153e0729097d0a
AngelV200 <140028970+AngelV200@users.noreply.github.com>
2023-08-16 22:23:07 -0700

    unit tests

---
+++ src/test/java/com/example/comp_380_project/HotelRoomsTest.java
@@ -0,0 +1,27 @@
+package com.example.comp_380_project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HotelRoomsTest {
+    Guest guest;
+    private static FileReader database;
+    String[] floor1 = {"Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open", "Open"}; //Small rooms
+
+    @Test
+    void reserveRoom() {
+        HotelRooms hotelRooms = new HotelRooms();
+        FileReader dataBase = new FileReader(hotelRooms);
+        hotelRooms.setDatabase(dataBase);
+        // Arrange
+
+        int floor = 1;
+        int roomNum = 2;
+        // Act
+        hotelRooms.reserveRoom(floor, roomNum);
+
+        // Assert
+        assertEquals("Closed", floor1[roomNum]);
+    }
+}
